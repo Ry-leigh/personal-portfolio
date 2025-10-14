c50f31dc266beff9ccf8d8745b1b46c5ae599c2e
Ry-leigh <ryanaarondc@gmail.com>
2025-10-14 15:44:35 +0800

    Feature: add new About Me section
    Style: modified template's default content and ids

--- src/App.jsx
+++ src/App.jsx
@@ -11,13 +11,16 @@ function App() {
                 <a href="#intro">Welcome</a>
               </li>
               <li>
-                <a href="#one">Who we are</a>
+                <a href="#about">About Me</a>
               </li>
               <li>
-                <a href="#two">What we do</a>
+                <a href="#skills">My Skillset</a>
               </li>
               <li>
-                <a href="#three">Get in touch</a>
+                <a href="#works">My Works</a>
+              </li>
+              <li>
+                <a href="#contact">Get in Touch</a>
               </li>
             </ul>
           </nav>
@@ -38,81 +41,23 @@ function App() {
             </p>
             <ul className="actions">
               <li>
-                <a href="#one" className="button scrolly">
+                <a href="#about" className="button scrolly">
                   Learn more
                 </a>
               </li>
             </ul>
           </div>
         </section>
-        {/* One */}
-        <section id="one" className="wrapper style2 spotlights">
-          <section>
-            <a href="#" className="image">
-              <img src="images/pic01.jpg" alt="" data-position="center center" />
-            </a>
-            <div className="content">
-              <div className="inner">
-                <h2>Sed ipsum dolor</h2>
-                <p>
-                  Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam
-                  turpis mauris, eu ultricies erat malesuada quis. Aliquam dapibus.
-                </p>
-                <ul className="actions">
-                  <li>
-                    <a href="generic.html" className="button">
-                      Learn more
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </section>
-          <section>
-            <a href="#" className="image">
-              <img src="images/pic02.jpg" alt="" data-position="top center" />
-            </a>
-            <div className="content">
-              <div className="inner">
-                <h2>Feugiat consequat</h2>
-                <p>
-                  Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam
-                  turpis mauris, eu ultricies erat malesuada quis. Aliquam dapibus.
-                </p>
-                <ul className="actions">
-                  <li>
-                    <a href="generic.html" className="button">
-                      Learn more
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </section>
-          <section>
-            <a href="#" className="image">
-              <img src="images/pic03.jpg" alt="" data-position="25% 25%" />
-            </a>
-            <div className="content">
-              <div className="inner">
-                <h2>Ultricies aliquam</h2>
-                <p>
-                  Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam
-                  turpis mauris, eu ultricies erat malesuada quis. Aliquam dapibus.
-                </p>
-                <ul className="actions">
-                  <li>
-                    <a href="generic.html" className="button">
-                      Learn more
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </section>
+        {/* About Me */}
+        <section id="about" className="wrapper style1 fade-up">
+          <div className="inner" style={{ paddingTop: 0 }}>
+            <h1 className="major" />
+            <h2>About Me</h2>
+          </div>
         </section>
-        {/* Two */}
-        <section id="two" className="wrapper style3 fade-up">
+
+        {/* My Skillset */}
+        <section id="skills" className="wrapper style3 fade-up">
           <div className="inner">
             <h2>What we do</h2>
             <p>
@@ -180,8 +125,76 @@ function App() {
             </ul>
           </div>
         </section>
-        {/* Three */}
-        <section id="three" className="wrapper style1 fade-up">
+
+        {/* My Works */}
+        <section id="works" className="wrapper style2 spotlights">
+          <section>
+            <a href="#" className="image">
+              <img src="images/pic01.jpg" alt="" data-position="center center" />
+            </a>
+            <div className="content">
+              <div className="inner">
+                <h2>Sed ipsum dolor</h2>
+                <p>
+                  Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam
+                  turpis mauris, eu ultricies erat malesuada quis. Aliquam dapibus.
+                </p>
+                <ul className="actions">
+                  <li>
+                    <a href="generic.html" className="button">
+                      Learn more
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </section>
+          <section>
+            <a href="#" className="image">
+              <img src="images/pic02.jpg" alt="" data-position="top center" />
+            </a>
+            <div className="content">
+              <div className="inner">
+                <h2>Feugiat consequat</h2>
+                <p>
+                  Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam
+                  turpis mauris, eu ultricies erat malesuada quis. Aliquam dapibus.
+                </p>
+                <ul className="actions">
+                  <li>
+                    <a href="generic.html" className="button">
+                      Learn more
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </section>
+          <section>
+            <a href="#" className="image">
+              <img src="images/pic03.jpg" alt="" data-position="25% 25%" />
+            </a>
+            <div className="content">
+              <div className="inner">
+                <h2>Ultricies aliquam</h2>
+                <p>
+                  Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam
+                  turpis mauris, eu ultricies erat malesuada quis. Aliquam dapibus.
+                </p>
+                <ul className="actions">
+                  <li>
+                    <a href="generic.html" className="button">
+                      Learn more
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </section>
+        </section>
+
+        {/* Get in Touch */}
+        <section id="contact" className="wrapper style1 fade-up">
           <div className="inner">
             <h2>Get in touch</h2>
             <p>
@@ -277,6 +290,7 @@ function App() {
           </div>
         </section>
       </div>
+      
       {/* Footer */}
       <footer id="footer" className="wrapper style1-alt">
         <div className="inner">
